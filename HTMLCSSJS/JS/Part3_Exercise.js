53f684366daa58ede798c16bb0ebf93ae6565731
Piyush4298 <piyushpande0405@gmail.com>
2021-07-27 23:57:33 +0530

    first commit

---
+++ HTMLCSSJS/JS/Part3_Exercise.js
@@ -0,0 +1,3 @@
+var wip = prompt("Enter weight in pounds(lbs)")
+alert("Weight in kgs -> " + wip*0.454)
+console.log("Conversion Completed") 
---
+++ HTMLCSSJS/JS/j1.js
@@ -0,0 +1,4 @@
+alert("Welcome backkkkk!!!")
+var age = prompt("Your age please")
+alert("ohh so you are "+age)
+console.log(age)
